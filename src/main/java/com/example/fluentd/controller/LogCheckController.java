96191bfc06a174ad014cf43bbfed9b601d81bc58
kazuki.kano <k.kazuki1021@gmail.com>
2021-07-11 21:16:44 +0900

    test workflows

--- src/main/java/com/example/fluentd/controller/LogCheckController.java
+++ src/main/java/com/example/fluentd/controller/LogCheckController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +20,24 @@ public class LogCheckController {
     private final LogCheckService logCheckService;
 
     @GetMapping("/check")
-    public ResponseEntity<String> logCheck(@RequestParam("id") int id) {
-        log.info("test message");
+    public ResponseEntity<String> check() {
+        log.info("Check info message");
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
 
+    @GetMapping("/check/{id}")
+    public ResponseEntity<String> checkId(@PathVariable("id") int id) {
+        return new ResponseEntity<>(logCheckService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/check/exception")
+    public ResponseEntity<String> exception() {
         try {
             throw new NullPointerException();
         } catch (Exception e) {
-            log.error("Test error message", e);
+            log.error("Check error message", e);
         }
-
-
-        return new ResponseEntity<>(logCheckService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
